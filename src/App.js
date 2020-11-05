4f4517738b158b084e5957b5a8af41c8391408e6
Mustafa Arif Şişman <mustafaarifsisman@gmail.com>
2020-11-05 23:55:56 +0300

    impl(wip): timer view

--- src/App.js
+++ src/App.js
@@ -1,56 +1,65 @@
-import { Row, Col, Container } from "react-bootstrap";
+import { Col } from "react-bootstrap";
 import { CountdownCircleTimer } from "react-countdown-circle-timer";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const renderTimer = (name, duration) => (
-  <Col xs={4} sm={4} md={4} lg={4} className="d-flex justify-content-center">
-    <h1>{name}</h1>
+  <Col className="d-flex justify-content-center" sm={12} md={6} lg={4}>
     <CountdownCircleTimer
       isPlaying
       duration={duration}
       onComplete={() => {
         return [true, 0];
       }}
-      size={250}
+      size={300}
+      strokeWidth={20}
       colors={[
         ["#004777", 0.33],
         ["#F7B801", 0.33],
         ["#A30000", 0.33],
       ]}
     >
-      {renderTime}
+      {({ remainingTime }) => renderTime(remainingTime, name)}
     </CountdownCircleTimer>
   </Col>
 );
 
-const renderTime = ({ remainingTime }) => {
+const renderTime = (remainingTime, name) => {
   const hours = Math.floor(remainingTime / 3600);
   const minutes = Math.floor((remainingTime % 3600) / 60);
   const seconds = remainingTime % 60;
 
+  let time = "";
+
+  if (remainingTime > 3600) {
+    time = `${hours}:${minutes}:${seconds}`;
+  } else if (remainingTime > 60) {
+    time = `${minutes}:${seconds}`;
+  } else {
+    time = `${seconds}`;
+  }
+
   return (
-    <div className="timer">
-      <h2>{`${hours}:${minutes}:${seconds}`}</h2>
+    <div>
+      <h1 className="text-center display-3">
+        <b>{name}</b>
+      </h1>
+      <h1 className="timer display-4">{time}</h1>
     </div>
   );
 };
 
 function App() {
   return (
-    <Container className="h-100">
-      <Row className="d-flex align-items-center my-auto">
-        {renderTimer("D1", 120)}
-        {renderTimer("H4", 120)}
-        {renderTimer("H1", 120)}
-      </Row>
+    <div className="mx-4 h-100 d-flex align-content-around flex-wrap">
+      {renderTimer("D1", 24 * 60 * 60)}
+      {renderTimer("H4", 4 * 60 * 60)}
+      {renderTimer("H1", 60 * 60)}
 
-      <Row className="d-flex align-items-center my-auto">
-        {renderTimer("M15", 120)}
-        {renderTimer("M5", 120)}
-        {renderTimer("M1", 120)}
-      </Row>
-    </Container>
+      {renderTimer("M15", 60 * 15)}
+      {renderTimer("M5", 60 * 5)}
+      {renderTimer("M1", 60)}
+    </div>
   );
 }
 
