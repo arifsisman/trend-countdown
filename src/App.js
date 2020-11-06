88ab4756e4107309a41ce597d5f1868b5f8d76e7
Mustafa Arif Şişman <mustafaarifsisman@gmail.com>
2020-11-06 03:16:31 +0300

    impl: styling

--- src/App.js
+++ src/App.js
@@ -3,69 +3,79 @@ import { CountdownCircleTimer } from "react-countdown-circle-timer";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const renderTimer = (name, duration) => (
+const renderTimer = (name, duration, remaining) => (
   <Col className="d-flex justify-content-center" sm={12} md={6} lg={4}>
     <CountdownCircleTimer
       isPlaying
       duration={duration}
+      initialRemainingTime={remaining}
       onComplete={() => {
         return [true, 0];
       }}
+      strokeLinecap={"square"}
+      trailColor={"#656969"}
       size={300}
       strokeWidth={20}
       colors={[
-        ["#004777", 0.33],
-        ["#F7B801", 0.33],
-        ["#A30000", 0.33],
+        ["#2a9100", 0.33],
+        ["#FFE135", 0.33],
+        ["#D82E3F", 0.33],
       ]}
     >
-      {({ remainingTime }) => renderTime(remainingTime, name)}
+      {({ remainingTime }) => renderCountdown(remainingTime, name)}
     </CountdownCircleTimer>
   </Col>
 );
 
-const renderTime = (remainingTime, name) => {
-  const hours = Math.floor(remainingTime / 3600);
-  const minutes = Math.floor((remainingTime % 3600) / 60);
-  const seconds = remainingTime % 60;
-
-  let time = "";
-
-  if (remainingTime > 3600) {
-    time = `${hours}:${minutes}:${seconds}`;
-  } else if (remainingTime > 60) {
-    time = `${minutes}:${seconds}`;
-  } else {
-    time = `${seconds}`;
-  }
-
+const renderCountdown = (remainingTime, name) => {
   return (
-    <div className="text-center ">
+    <div className="text-center">
       <h1 className="display-4 font-weight-bold">{name}</h1>
-      <h1 className="timer font-weight-bold">{time}</h1>
+      <h1 className="font-weight-bold">
+        {remainingTime.toHHMMSS()}
+        {/* {new Date(remainingTime * 1000).toISOString().substr(11, 8)} */}
+      </h1>
     </div>
   );
 };
 
+// eslint-disable-next-line no-extend-native
+Number.prototype.toHHMMSS = function () {
+  var hours = Math.floor(this / 3600);
+  var minutes = Math.floor((this - hours * 3600) / 60);
+  var seconds = this - hours * 3600 - minutes * 60;
+
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+  return hours + ":" + minutes + ":" + seconds;
+};
+
 function App() {
-  const d1 = 24 * 60 * 60;
-  const h4 = 4 * 60 * 60;
-  const h1 = 60 * 60;
-  const m15 = 15 * 60;
-  const m5 = 5 * 60;
   const m1 = 60;
+  const m5 = 5 * m1;
+  const m15 = 15 * m1;
+  const h1 = 60 * m1;
+  const h4 = 4 * h1;
+  const d1 = 24 * h1;
 
   const dt = new Date();
   const now = dt.getSeconds() + 60 * dt.getMinutes() + 60 * 60 * dt.getHours();
 
   return (
     <div className="mx-4 h-100 d-flex align-content-around flex-wrap">
-      {renderTimer("D1", d1 - now)}
-      {renderTimer("H4", h4 - (now % h4))}
-      {renderTimer("H1", h1 - (now % h1))}
-      {renderTimer("M15", m15 - (now % m15))}
-      {renderTimer("M5", m5 - (now % m5))}
-      {renderTimer("M1", m1 - (now % m1))}
+      {renderTimer("D1", d1, d1 - now)}
+      {renderTimer("H4", h4, h4 - (now % h4))}
+      {renderTimer("H1", h1, h1 - (now % h1))}
+      {renderTimer("M15", m15, m15 - (now % m15))}
+      {renderTimer("M5", m5, m5 - (now % m5))}
+      {renderTimer("M1", m1, m1 - (now % m1))}
     </div>
   );
 }
